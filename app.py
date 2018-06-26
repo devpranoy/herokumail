3b68420fdbb01e11540f424894574ccd87eb60f0
Pranoy Dev <devpranoy@gmail.com>
2018-06-26 21:16:52 +0530

    Email bot

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask ,render_template, flash, redirect, url_for, session, request, logging
+import smtplib
+import time
+from email.Header import Header
+from email.mime.text import MIMEText
 
 app = Flask(__name__)
 
@@ -9,8 +13,28 @@ def send():
 		email = request.form['email']					#GET FORM FIELDS
 		subject= request.form['subject']
 		message = request.form['message']
-		return message
+		me = 'sunil@alamrigroup.org' # change to your email
+		p_reader = open('password.txt', 'rb') # edit for your password
+		cipher = p_reader.read()
+		recipients = ['sunil@alamrigroup.org','devpranoy@gmail.com'] # enter recipients here
 
+        #multipart class is for multiple recipients
+        msg = message
+        msg['Subject'] = Header('Message from alamrigroup.org website', 'utf-8')
+        msg['From'] = me
+        msg['To'] = ', '.join(recipients)
+
+        s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        s.login(me, cipher)
+        s.sendmail(me, recipients, msg.as_string())
+
+        print "Email sent to: " + ', '.join(recipients)
+        s.quit()
+        return"200 Success"
+	return "200"
 
 @app.route('/', methods=['GET','POST'])
 def index():
